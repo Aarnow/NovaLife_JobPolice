5b8628161b1dba734b655df2287fc39176ea7e21
Aarnow <arnaudfgr.dev@gmail.com>
2024-03-27 19:36:01 +0100

    Initial Project Files

---
+++ JobPolice.cs
@@ -0,0 +1,31 @@
+﻿using Life;
+using ModKit.Helper;
+using ModKit.Interfaces;
+using ModKit.Internal;
+
+namespace JobPolice
+{
+    public class JobPolice : ModKit.ModKit
+    {
+        public JobPolice(IGameAPI api) : base(api)
+        {
+            PluginInformations = new PluginInformations(AssemblyHelper.GetName(), "1.0.0", "Aarnow");
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            InitEntities();
+            InsertMenu();
+            Logger.LogSuccess($"{PluginInformations.SourceName} v{PluginInformations.Version}", "initialisé");
+        }
+
+        public void InitEntities()
+        {
+        }
+
+        public void InsertMenu()
+        {
+        }
+    }
+}
